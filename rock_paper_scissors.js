3435bb962a7aabf969c5b61dc16777285368a2c3
t3t3c <kleyguy@gmail.com>
2022-05-20 09:48:55 +0200

    Added playRound() and makeCaseInsetentive() functions

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,5 +1,3 @@
-
-
 function computerPlay() {
     let randomNumber = Math.random() * 3; // *3 to get value between 0 and 3
     if (randomNumber <= 1) {
@@ -11,6 +9,7 @@ function computerPlay() {
     }
 
 }
+
 function checkIfRandom(howMuch) {
     let rock = paper = scissors = 0;
     for (let i = 0; i < howMuch; i++) {
@@ -29,3 +28,20 @@ function checkIfRandom(howMuch) {
     let lista = [rock, paper, scissors]
     return lista
 }
+
+function makeCaseInsetentive(userInput) {
+    return userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a draw! You both chose ${computerSelection}`;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+        playerSelection === 'Paper' && computerSelection === 'Rock' ||
+        playerSelection == - 'Scissors' && computerSelection === 'Paper') {
+        return `You win! Your ${playerSelection} beats ${computerSelection}`;
+    } else { // Computer wins
+        return `You loose! Computer's ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
